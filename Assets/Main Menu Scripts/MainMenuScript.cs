57c5979b79964a153b3b0993497a61b20c18f3f2
PatrikB-hub <pattty306@gmail.com>
2020-11-12 16:41:11 +1100

    adding menu res and quality

--- Assets/Main Menu Scripts/MainMenuScript.cs
+++ Assets/Main Menu Scripts/MainMenuScript.cs
@@ -6,9 +6,52 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    #region Variables
+
     [SerializeField, Tooltip("fullscreen toggle")]
     private Toggle fullscreenToggle;
+    [SerializeField, Tooltip("Game Quailty Dropdown")]
+    private Dropdown qualityDropdown;
+    [SerializeField, Tooltip("")]
+    private Dropdown resolution;
+    private Resolution[] resolutions;
 
+    #endregion
+
+    private void Start()
+    {
+        GetResolutions();
+
+        LoadPlayerPrefs();
+
+        if (!PlayerPrefs.HasKey("fullscreen"))
+        {
+            PlayerPrefs.SetInt("fullscreen", 0);
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("fullscreen") == 0)
+            {
+                Screen.fullScreen = false;
+            }
+            else
+            {
+                Screen.fullScreen = true;
+            }
+        }
+
+        if (!PlayerPrefs.HasKey("quality"))
+        {
+            PlayerPrefs.SetInt("quality", 6);// the highest quality setting
+            QualitySettings.SetQualityLevel(6);
+        }
+        else
+        {
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality"));
+        }
+        PlayerPrefs.Save();
+    }
 
     #region QuitMethod
     public void QuitGame()
@@ -20,10 +63,35 @@ public class MainMenuScript : MonoBehaviour
     }
     #endregion
 
+    #region Change Settings
+
+    public void ChangeQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, false);
+    }
+
+    #endregion
 
     #region Save and Load PlayerPrefs
     public void LoadPlayerPrefs()
     {
+        //load quality
+        if (PlayerPrefs.HasKey("quality"))
+        {
+            int quality = PlayerPrefs.GetInt("quality");
+            qualityDropdown.value = quality;
+            if (QualitySettings.GetQualityLevel() != quality)
+            {
+                ChangeQuality(quality);
+            }
+        }
+
         //load fullscreen
         if (PlayerPrefs.GetInt("fullscreen") == 0)
         {
@@ -39,6 +107,9 @@ public class MainMenuScript : MonoBehaviour
 
     public void SavePlayerPrefs()
     {
+        //save quality
+        PlayerPrefs.SetInt("quality", QualitySettings.GetQualityLevel());
+
         // save full screen
         if (fullscreenToggle.isOn)
         {
@@ -48,8 +119,35 @@ public class MainMenuScript : MonoBehaviour
         {
             PlayerPrefs.SetInt("fullscreen", 0);
         }
-
     }
     #endregion
+
+    /// <summary>
+    /// find all of the resolutions for the current screen
+    /// </summary>
+    private void GetResolutions()
+    {
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)// go through every resolution
+        {
+            //build a string for displaying the resolutions
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        // set up the dropdown
+        resolution.AddOptions(options);
+        resolution.value = currentResolutionIndex;
+        resolution.RefreshShownValue();
+    }
+
 
 }
